f7d8a837e95483dc4780fd710c5ba3b50523d896
Jonathan Potter <jonathan@mrjonathanpotter.com>
2021-12-14 14:06:18 -0800

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+ name: "JoJo",
+ streetAddress: "123 CandyCane Lane", 
+}
+
+//4 functions
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeUpdated = { ...employee };
+    employeeUpdated[key] = value;
+    return employeeUpdated;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    //console.log(employee);
+    return employee;
+}
+//destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Poop");
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeLessKey = {...employee};
+    delete employeeLessKey[key];
+    console.log(employeeLessKey);
+    return employeeLessKey;
+}
+
+//deleteFromEmployeeByKey(employee, "streetAddress");
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee
+}
